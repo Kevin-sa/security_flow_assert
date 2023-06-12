bd50472faf47474e1db48e6c924e851c89bef302
kevinsa <kevinsafs@gmail.com>
2023-06-12 22:40:36 +0800

    update

--- burpsuite_plugin/src/main/java/com/kevinsa/assertlog/BurpExtender.java
+++ burpsuite_plugin/src/main/java/com/kevinsa/assertlog/BurpExtender.java
@@ -3,15 +3,12 @@ package main.java.com.kevinsa.assertlog;
 import burp.*;
 
 import java.io.PrintWriter;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
-public class BurpExtender implements IBurpExtender, IExtensionStateListener {
+public class BurpExtender implements IBurpExtender, IProxyListener {
+
+
     private IBurpExtenderCallbacks callbacks;
-
     private final static String ExtenderName = "sec_assert_log";
-
     private PrintWriter stdout;
     private PrintWriter stderr;
 
@@ -24,31 +21,43 @@ public class BurpExtender implements IBurpExtender, IExtensionStateListener {
 
         this.callbacks = iBurpExtenderCallbacks;
         callbacks.setExtensionName(ExtenderName);
-        callbacks.registerExtensionStateListener(BurpExtender.this);
 
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-
-        executorService.schedule(this::flowDataCollect, 60, TimeUnit.SECONDS);
+        callbacks.registerProxyListener(BurpExtender.this);
     }
-
-//    @Override
-//    public void processHttpMessage(int i, boolean b, IHttpRequestResponse iHttpRequestResponse) {
-//        if (i == IBurpExtenderCallbacks.TOOL_PROXY) {
-//            if (b) {
-//                IRequestInfo iRequestInfo = helpers.analyzeRequest(iHttpRequestResponse);
-//                stdout.println(iRequestInfo);
-//                IResponseInfo iResponseInfo = helpers.analyzeResponse(iHttpRequestResponse.getResponse());
-//                stdout.println(iResponseInfo);
-//            }
-//        }
-//    }
 
     @Override
-    public void extensionUnloaded() {
+    public void processProxyMessage(boolean b, IInterceptedProxyMessage iInterceptedProxyMessage) {
+        IExtensionHelpers helpers = callbacks.getHelpers();
+        IHttpRequestResponse iHttpRequestResponse = iInterceptedProxyMessage.getMessageInfo();
+        if (b) {
+            stdout.println("request");
+            if (iHttpRequestResponse.getRequest() != null) {
+                IRequestInfo iRequestInfo = helpers.analyzeRequest(iHttpRequestResponse.getRequest());
+                String request = new String(iHttpRequestResponse.getRequest());
+                stdout.println("request url" + iRequestInfo.getUrl());
+            }
+        } else {
+            if (iHttpRequestResponse.getResponse() != null) {
+                IResponseInfo iResponseInfo = helpers.analyzeResponse(iHttpRequestResponse.getResponse());
+//                String response = new String(iHttpRequestResponse.getResponse());
+                stdout.println("response status" + iResponseInfo.getStatusCode());
 
-    }
+                byte[] responseBody = iHttpRequestResponse.getResponse();
+                int bodyOffset = iResponseInfo.getBodyOffset();
 
-    private void flowDataCollect() {
+                // 获取响应消息体
+                byte[] responseBody = new byte[iHttpRequestResponse.getResponse().length - bodyOffset];
+                System.arraycopy(iHttpRequestResponse.getResponse(), bodyOffset, responseBody, 0, responseBody.length);
 
+                // 将消息体转换为字符串
+                String responseBodyString = helpers.bytesToString(responseBody);
+
+                // 处理响应消息体
+                System.out.println("Response Body: " + responseBodyString);
+            }
+            stdout.println("response");
+        }
     }
 }
+
+
